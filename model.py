a36dbc82b1a21a3dde433cb296af36b55795779c
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-19 18:47:42 +0100

    create the database model

---
+++ model.py
@@ -0,0 +1,9 @@
+from extensions import db
+
+class Student(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), unique=True, nullable=False)
+  mail = db.Column(db.VARCHAR(200), unique=True, nullable=False)
+  cohort = db.Column(db.String(80), unique=False, nullable=False)
+  program = db.Column(db.String(100), unique=False, nullable=False)
+  status = db.Column(db.String(80), unique=False, nullable=False)
