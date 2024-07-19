89b39daa5395ee911ae836199da912da50bc5f00
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-19 18:01:34 +0100

    create simple route

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/register', methods=['POST'])
+def register():
+  pass
+
+@app.route('/checkin', methods=['POST'])
+def checkin():
+  pass
+
+@app.route('/checkout', methods=['POST'])
+def checkout():
+  pass
+
+@app.route('/status', methods=['POST'])
+def status():
+  pass
